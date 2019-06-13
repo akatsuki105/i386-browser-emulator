b84c62ef287e62edebd91295b453d903cd50b1e8
Akatsuki-py <bluejapan73+dev@gmail.com>
2019-06-13 15:00:31 +0900

    🚧 :  WIP

---
+++ index.js
@@ -0,0 +1,72 @@
+const MEMOEY_SIZE = 1024 * 1024;
+
+let memory = [];
+
+const Register = {
+    "EAX": 0,
+    "ECX": 1,
+    "EDX": 2,
+    "EBX": 3,
+    "ESP": 4,
+    "EBP": 5,
+    "ESI": 6,
+    "EDI": 7,
+    "REGISTERS_COUNT": 8,
+    "AL": 0,
+    "CL": 1,
+    "DL": 2,
+    "BL": 3,
+    "AH": 5,
+    "CH": 6,
+    "DH": 7,
+    "BH": 8
+};
+
+const registersName = ["EAX", "ECX", "EDX", "EBX", "ESP", "EBP", "ESI", "EDI"];
+
+class Emulator {
+    constructor(eip, esp) {
+        this.registers = [0, 0, 0, 0, 0, 0, 0, 0];
+        this.eflags = 0;
+        this.memory = (new Array(MEMOEY_SIZE)).fill(0);
+        this.eip = eip;
+        this.registers[Register.ESP] = esp;
+    }
+}
+
+function onAddFile(event) {
+    var files;
+    var reader = new FileReader();
+
+    if (event.target.files) {
+        files = event.target.files;
+    } else {
+        files = event.dataTransfer.files;
+    }
+
+    reader.onload = function (event) {
+        var raw = new Uint8Array(reader.result);
+        let size = raw.length;
+        for (let i = 0; i < size; i++) {
+            memory.push(raw[i]);
+        }
+        mainFunc();
+    };
+    if (files[0]) {
+        reader.readAsArrayBuffer(files[0]);
+        document.getElementById("inputfile").value = '';
+    }
+}
+
+function readBinary(emu, memory) {
+    let size = memory.length;
+    for (let i = 0; i < size; i++) {
+        emu.memory[0x7c00 + i] = memory[i]; 
+    }
+}
+
+function mainFunc() {
+    emu = new Emulator(0x7c00, 0x7c00);
+    readBinary(emu, memory);
+    console.log(emu);
+}
--- instruction.js
+++ instruction.js
@@ -1,5 +1,12 @@
+let instructions = (new Array(256)).fill(0);
+
 function movR8Imm8(emu)
 {
     console.log("hello");
 }
 
+for (i = 0; i < 8; i++) {
+    instructions[0xB0 + i] = movR8Imm8;
+}
+
+export default instructions;
